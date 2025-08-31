b9f123430529c4dc2da47affbe8d3430f513e9ae
Pablo <andresmanjarresneg@gmail.com>
2025-08-31 11:16:28 -0500

    cars

---
+++ practiceII.pl
@@ -0,0 +1,22 @@
+% vehicle(Brand, Reference, Type, Price, Year).
+
+vehicle(ferrari, 'F430', coupe, 180000, 2008).
+vehicle(bmw, '7 Series E65', sedan, 90000, 2006).
+vehicle(mercedes, 'S-Class W220', sedan, 95000, 2005).
+vehicle(audi, 'A8 D3', sedan, 85000, 2007).
+vehicle(porsche, '911 Carrera 997', coupe, 120000, 2009).
+vehicle(rolls_royce, 'Phantom VIII', sedan, 450000, 2023).
+vehicle(bentley, 'Continental GT', coupe, 250000, 2022).
+vehicle(ferrari, 'SF90 Stradale', coupe, 500000, 2021).
+vehicle(lamborghini, 'Urus', suv, 230000, 2022).
+vehicle(porsche, 'Taycan Turbo S', sedan, 185000, 2021).
+vehicle(bmw, 'i7', sedan, 150000, 2023).
+vehicle(mercedes, 'Maybach S680', sedan, 220000, 2022).
+vehicle(audi, 'RS7 Sportback', sedan, 140000, 2021).
+vehicle(maserati, 'MC20', coupe, 210000, 2022).
+vehicle(aston_martin, 'DB11', coupe, 200000, 2020).
+vehicle(jaguar, 'I-Pace', suv, 85000, 2021).
+vehicle(tesla, 'Model S Plaid', sedan, 130000, 2023).
+vehicle(cadillac, 'Escalade V', suv, 115000, 2023).
+vehicle(lexus, 'LS 500', sedan, 105000, 2022).
+vehicle(genesis, 'G90', sedan, 98000, 2023).
